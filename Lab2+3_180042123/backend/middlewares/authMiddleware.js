e1351ff48407f83de3de445c3d950c06bf19cc1c
rtlmhjbn <ratulmahjabin@iut-dhaka.edu>
2021-06-26 01:38:02 +0600

    middleware added

---
+++ Lab2+3_180042123/backend/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(cookieParser());
+
+var LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./scratch');
+const alert = require('alert');
+
+
+
+const isLoggedIn = (req, res, next) => {
+
+    const user = localStorage.getItem("fullname");
+
+    if (user) {
+        res.clearCookie('user');
+        res.cookie("user", user);
+        alert('welcome ', user);
+        next();
+    } else {
+        alert('Please log in first');
+        res.redirect('/login');
+    }
+};
+
+module.exports = isLoggedIn;

2cf71dcd21fc4dd61d619b06409cde8d2131791f
rtlmhjbn <ratulmahjabin@iut-dhaka.edu>
2021-06-25 16:46:45 +0600

    Database connection

--- Lab2+3_180042123/backend/index.js
+++ Lab2+3_180042123/backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = require('./app');
+const mongoose = require('mongoose');
 const port = process.env.PORT;
 
 
@@ -8,3 +9,7 @@ const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`server is running at port ${port}`);
 });
+
+mongoose.connect(process.env.Database_token, () => {
+    console.log("Database connected");
+}).catch((error) => console.log(error.message));

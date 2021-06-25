c816355591c30c80ba75f26fb48f6012b4293b0a
rtlmhjbn <ratulmahjabin@iut-dhaka.edu>
2021-06-25 15:20:53 +0600

    adding url

--- Lab2+3_180042123/backend/app.js
+++ Lab2+3_180042123/backend/app.js
@@ -5,4 +5,20 @@ app.get('/', (req, res) => {
     res.send("<H1> Home page </H1>");
 });
 
+app.get("/dashboard", (req, res) => {
+    res.send("<H1>Dashboard</H1>");
+});
+
+app.get("/login", (req, res) => {
+    res.send("<H1>Login Page</H1>");
+});
+
+app.get("/register", (req, res) => {
+    res.send("<H1>Register Page</H1>");
+});
+
+app.use((req, res) => {
+    res.send("Page doesn't exist!");
+});
+
 module.exports = app;

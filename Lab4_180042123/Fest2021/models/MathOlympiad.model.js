ad11146fc2292618ccb4450e4ddcfcf198b2b349
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-04 22:55:03 +0600

    Create MathOlympiad.model.js

---
+++ Lab4_180042123/Fest2021/models/MathOlympiad.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const mathOlympiadSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  institution: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  paid: {
+    type: Number,
+    required: true,
+  },
+  tshirt: {
+    type: String,
+    required: true,
+  },
+  selected: {
+    type: Boolean,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const MathOlympiad = mongoose.model('math-olympiad', mathOlympiadSchema)
+module.exports = MathOlympiad

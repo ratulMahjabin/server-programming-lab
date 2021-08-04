af41a33e416e3df652aaa3655b91ae91683a0a80
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-05 00:36:14 +0600

    List added

--- Lab4_180042123/Fest2021/controllers/mathOlympiad.controller.js
+++ Lab4_180042123/Fest2021/controllers/mathOlympiad.controller.js
@@ -52,8 +52,26 @@ const postMO = (req, res) => {
 }
 
 const getMOList = (req, res) => {
-  res.render('math-olympiad/list.ejs')
+  let all_participant = []
+  let error = ''
+  MathOlympiad.find()
+    .then((data) => {
+      all_participant = data
+      res.render('math-olympiad/list.ejs', {
+        error: req.flash('error'),
+        participants: all_participant,
+      })
+    })
+    .catch(() => {
+      error = 'Failed to fetch data!'
+      res.render('math-olympiad/list.ejs', {
+        error: req.flash('error', error),
+        participants: all_participant,
+      })
+    })
+  // res.render("math-olympiad/list.ejs");
 }
+
 const deleteMO = (req, res) => {
   const id = req.params.id
   console.log(id)

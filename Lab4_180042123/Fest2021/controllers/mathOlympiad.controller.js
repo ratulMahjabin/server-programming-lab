c83cce1632f026914aeca20c0b2954659dc132e4
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-04 23:10:24 +0600

    Create mathOlympiad.controller.js

---
+++ Lab4_180042123/Fest2021/controllers/mathOlympiad.controller.js
@@ -0,0 +1,20 @@
+const MathOlympiad = require('../models/MathOlympiad.model')
+
+const getMO = (req, res) => {
+  res.render('math-olympiad/register.ejs')
+}
+
+const postMO = (req, res) => {
+  res.render('math-olympiad/register.ejs')
+}
+
+const getMOList = (req, res) => {
+  res.render('math-olympiad/list.ejs')
+}
+const deleteMO = (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  res.render('math-olympiad/register.ejs')
+}
+
+module.exports = { getMO, postMO, getMOList, deleteMO }

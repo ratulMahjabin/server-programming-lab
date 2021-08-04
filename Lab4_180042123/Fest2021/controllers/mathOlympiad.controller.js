ce452c44eac63022c0aa1e45cc5351c1e1647797
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-05 00:02:50 +0600

    post register added in controller

--- Lab4_180042123/Fest2021/controllers/mathOlympiad.controller.js
+++ Lab4_180042123/Fest2021/controllers/mathOlympiad.controller.js
@@ -1,11 +1,57 @@
 const MathOlympiad = require('../models/MathOlympiad.model')
-
 const getMO = (req, res) => {
   res.render('math-olympiad/register.ejs')
 }
 
 const postMO = (req, res) => {
-  res.render('math-olympiad/register.ejs')
+  const { name, category, contact, email, institution, tshirt } = req.body
+  console.log(institution)
+  let registrationFee = 0
+  if (category == 'School') {
+    registrationFee = 250
+  } else if (category == 'College') {
+    registrationFee = 400
+  } else {
+    registrationFee = 500
+  }
+  const total = registrationFee
+  const paid = 0
+  const selected = false
+  let error = ''
+
+  MathOlympiad.findOne({ name: name, contact: contact }).then((participant) => {
+    if (participant) {
+      error = 'Participant with this name and contact already exists!'
+
+      req.flash('error', error)
+      res.redirect('/MathOlympiad/register')
+    } else {
+      const participant = new MathOlympiad({
+        name,
+        category,
+        contact,
+        email,
+        institution,
+        paid,
+        total,
+        selected,
+        tshirt,
+      })
+      participant
+        .save()
+        .then(() => {
+          error = 'Participant has been registered successfully!!'
+          console.log(error)
+          res.redirect('/MathOlympiad/register')
+        })
+        .catch(() => {
+          error = 'Unexpected error occured while registering!'
+          console.log(error)
+          res.redirect('/MathOlympiad/register')
+        })
+    }
+  })
+  //   res.render('math-olympiad/register.ejs')
 }
 
 const getMOList = (req, res) => {
@@ -14,7 +60,6 @@ const getMOList = (req, res) => {
 const deleteMO = (req, res) => {
   const id = req.params.id
   console.log(id)
-  res.render('math-olympiad/register.ejs')
+  res.render('math-olympiad/list.ejs')
 }
-
 module.exports = { getMO, postMO, getMOList, deleteMO }

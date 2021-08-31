4e8b356e447c6500046c1ad6d89c0e3ecc53f696
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-31 23:55:31 +0600

    email sent done in MO

--- Lab4_180042123/Fest2021/controllers/mathOlympiad.controller.js
+++ Lab4_180042123/Fest2021/controllers/mathOlympiad.controller.js
@@ -1,3 +1,13 @@
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  service: 'hotmail',
+  auth: {
+    user: 'iutict2021@outlook.com',
+    pass: 'z+<=i=TX7M;W,2d',
+  },
+})
+
 const MathOlympiad = require('../models/MathOlympiad.model')
 const getMO = (req, res) => {
   res.render('math-olympiad/register.ejs', { error: req.flash('error') })
@@ -16,6 +26,7 @@ const postMO = (req, res) => {
   const total = registrationFee
   const paid = 0
   const selected = false
+  var val = Math.floor(1000 + Math.random() * 9000)
   let error = ''
 
   MathOlympiad.findOne({ name: name, contact: contact }).then((participant) => {
@@ -34,11 +45,32 @@ const postMO = (req, res) => {
         total,
         selected,
         tshirt,
+        confirmationCode: val,
       })
       participant
         .save()
         .then(() => {
           error = 'Participant has been registered successfully!!'
+
+          const options = {
+            to: email,
+            from: 'iutict2021@outlook.com',
+            subject: 'Registration is Successful!',
+            text:
+              'Dear ' +
+              name +
+              ', \n' +
+              'Congratulations! Your Registration to Math Olympiad in ICT Fest, 2021 is successful.',
+          }
+
+          transporter.sendMail(options, function (err, info) {
+            if (err) {
+              console.log(err)
+              return
+            }
+            console.log('Sent: ' + info.response)
+          })
+
           req.flash('error', error)
           res.redirect('/MathOlympiad/register')
         })
@@ -161,17 +193,37 @@ const selectMO = (req, res) => {
         .save()
         .then(() => {
           let error = 'Participant has been selected succesfully'
+
+          const options = {
+            to: participant.email,
+            from: 'iutict2021@outlook.com',
+            subject: 'You have been Selected!',
+            text:
+              'Dear ' +
+              participant.name +
+              ', \n' +
+              'Congratulations! Your have been selected for the Math Olympiad in ICT Fest, 2021 is successful.',
+          }
+
+          transporter.sendMail(options, function (err, info) {
+            if (err) {
+              console.log(err)
+              return
+            }
+            console.log('Sent: ' + info.response)
+          })
+
           req.flash('error', error)
           res.redirect('/MathOlympiad/list')
         })
         .catch(() => {
-          let error = 'Data could not be updated'
+          let error = "Error Occured Participant couldn't be selected"
           req.flash('error', error)
           res.redirect('/MathOlympiad/list')
         })
     })
     .catch(() => {
-      let error = 'Data could not be updated'
+      let error = 'Unknown Error occured and Participant was not found.'
       req.flash('error', error)
       res.redirect('/MathOlympiad/list')
     })
